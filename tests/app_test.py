94df6ed21b5567e2b67ddad73978c9c22e3d1992
gluap <git@pgoergen.de>
2020-03-29 16:48:54 +0200

    add smoke tests for plots (must return svg xml)

--- tests/app_test.py
+++ tests/app_test.py
@@ -18,7 +18,7 @@ def test_stats(country):
 def test_predictions(country):
     p = app.predictions(country)
     assert not p['error']
-    assert isinstance(p['t2'],float)
+    assert isinstance(p['t2'], float)
 
 
 @pytest.mark.parametrize("country", countries)
@@ -32,7 +32,7 @@ def test_get_and_fit(country):
 @pytest.mark.vcr()
 def test_get_and_fit(country):
     gf = app.timeseries_data(country)
-    assert gf[0][-1]>0
+    assert gf[0][-1] > 0
     assert gf[2][-1] > 0
 
 
@@ -50,3 +50,21 @@ def test_estimate_from_daily(country):
     gf = app.estimate_from_daily(country)
     assert isinstance(gf[1][0], datetime.datetime)
     assert gf[0][-1] > 0
+
+
+@pytest.mark.parametrize("country", countries)
+@pytest.mark.vcr()
+def test_plots(country):
+    p1 = app.plot(country)
+    p2 = app.plot_deathrate_vs_detection(country)
+    p3 = app.plot_doublingtime_estimates(country)
+    p4 = app.plot_deaths_per_confirmed(country)
+    assert p1.startswith(b"<?xml version")
+    assert p2.startswith(b"<?xml version")
+    assert p3.startswith(b"<?xml version")
+    assert p4.startswith(b"<?xml version")
+
+
+def test_get_tld():
+    de = app.get_tld("Germany")
+    assert de == de
