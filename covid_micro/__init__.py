cfbd0f4e63c874b6bf58d759fec5da612e6b0444
paulg <github@pgoergen.de>
2020-04-18 20:57:49 +0200

    add first plot for german Kreise

--- covid_micro/__init__.py
+++ covid_micro/__init__.py
@@ -4,12 +4,12 @@ import os.path
 from io import StringIO
 
 from flask import Flask, Response
-
 from flask_apscheduler import APScheduler
 
-
 from covid_micro.app import plot, predictions, logger, get_cached, URL_TIMESERIES_CONFIRMED, \
-    plot_doublingtime_estimates, plot_deathrate_vs_detection, plot_deaths_per_confirmed, load_countries, plot_daily_infected
+    plot_doublingtime_estimates, plot_deathrate_vs_detection, plot_deaths_per_confirmed, load_countries, \
+    plot_daily_infected
+from covid_micro.germany import plot_kreis, get_data_by_name, number_by_name
 
 
 def create_app():
@@ -102,6 +102,32 @@ def create_app():
             logger.exception("exception when making html")
             return Response(ERROR.format(country=country, exception=exc), mimetype="text/html")
         return Response(HTML.format(**data), mimetype='text/html')
+
+    @app.route('/de/<kreis>.svg')
+    def plotout_kreis(kreis="Darmstadt", cache={}):
+        if kreis not in cache or (
+                datetime.datetime.now() - cache[kreis]['timestamp'] > datetime.timedelta(minutes=15)):
+            cache[kreis] = {'data': (plot_kreis(kreis)),
+                            'timestamp': datetime.datetime.now()}
+        return Response(cache[kreis]['data'], mimetype='image/svg+xml')
+
+    @app.route('/de/<kreis>')
+    def html_kreis(kreis="Darmstadt"):
+        try:
+            raw_data = get_data_by_name(kreis)
+            data = {'kreis': kreis, **raw_data[2]['currentStats'], **raw_data[0]}
+        except Exception as exc:
+            logger.exception("exception when making html")
+            return Response(ERROR.format(country=kreis, exception=exc), mimetype="text/html")
+        return Response(HTML_KREIS.format(**data), mimetype='text/html')
+
+    @app.route('/de/')
+    @app.route('/de/index.html')
+    def index_de():
+        kreise = number_by_name.keys()
+
+        return Response(
+            HTML_COUNTRIES.format(countries="<LI>".join([f'<a href="{c}">{c}</a>' for c in sorted(kreise)])))
 
     return app
 
@@ -196,3 +222,40 @@ ERROR = """
 There was a problem fetching {country}. {exception}
 </body>
 </html>"""
+
+HTML_KREISE = """
+<HTML>
+<head profile="http://www.w3.org/2005/10/profile">
+<link rel="icon" 
+      type="image/png" 
+      href="favicon.png">
+
+<title>Landkreise</title>
+</head><Body>
+<ul>
+<LI/>{kreise}
+</ul>
+</body>
+</html>
+"""
+
+HTML_KREIS = """
+<HTML>
+<head profile="http://www.w3.org/2005/10/profile">
+<link rel="icon" 
+      type="image/png" 
+      href="favicon.png">
+
+<title>{kreis}</title>
+</head><Body>
+<H1>{kreis}</h1>
+<B>Bundesland:</B> {bundesland}
+<B>Einwohner:</B> {population}
+<B>Fälle:</B> {count}
+<B>davon genesen:</B> {recovered}
+<B>verstorben:</B> {dead}
+<B>active:</B> {active}
+<IMG style="vertical-align:top" SRC="{kreis}.svg">
+</body>
+</html>
+"""
